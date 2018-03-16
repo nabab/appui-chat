cbc89279511b28807741b4fc0c915ef1b2535cf8
nabab <thomas.nabet@gmail.com>
2018-03-16 14:17:36 +0100

    Changed message action

--- src/mvc/model/actions/message.php
+++ src/mvc/model/actions/message.php
@@ -6,9 +6,14 @@
 
 /** @var $this \bbn\mvc\model*/
 $res = ['success' => false];
-if ( isset($model->data['message']) && (!empty($model->data['id']) || !empty($model->data['user'])) ){
-  $chat = new \bbn\appui\chat($model->db, $model->inc->user);
-  $id_chat = empty($model->data['id']) ? $chat->get_chat_by_user($model->data['user']) : $model->data['id'];
+if ( isset($model->data['message']) && (!empty($model->data['id']) || !empty($model->data['users'])) ){
+  if ( empty($model->data['id']) ){
+    $chat = new \bbn\appui\chat($model->db, $model->inc->user);
+    $id_chat = empty($model->data['id']) ? $chat->get_chat_by_users($model->data['users']) : $model->data['id'];
+  }
+  else{
+    $id_chat = $model->data['id'];
+  }
   if ( $id_chat ){
     $chat->talk($id_chat, $model->data['message']);
     $res['success'] = true;
