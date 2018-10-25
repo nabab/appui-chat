fdc5040b4be79324f14eef0056c4db34226880ab
nabab <thomas.nabet@gmail.com>
2018-10-25 17:41:05 +0200

    Started chat actions

---
+++ src/mvc/model/actions/add_user.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BBN
+ * Date: 24/10/2018
+ * Time: 14:37
+ */
+
+if ( isset($model->data['id_chat'], $model->data['id_user']) ){
+  return [
+    'success' => $model->inc->chat->add_user($model->data['id_chat'], $model->data['id_user'])
+  ];
+}
--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -7,4 +7,5 @@
 /** @var $this \bbn\mvc\controller */
 if ( !\defined('APPUI_CHAT_ROOT') ){
   define('APPUI_CHAT_ROOT', $ctrl->plugin_url('appui-chat').'/');
+  $ctrl->add_inc('chat', new \bbn\appui\chat($ctrl->db, $ctrl->inc->user));
 }
