880e147d7f84d153a48dae41965785cee86182ef
Thomas Nabet <thomas.nabet@gmail.com>
2020-10-01 04:36:24 +0200

    Fixes in time float

--- src/mvc/model/actions/message.php
+++ src/mvc/model/actions/message.php
@@ -6,6 +6,11 @@
 
 /** @var $this \bbn\mvc\model*/
 $res = ['success' => false];
+if ( !empty($model->data['id_chat']) && !empty($model->data['time']) ){
+  return [
+    'messages' => $model->inc->chat->get_prev_messages($model->data['id_chat'], $model->data['time'])
+  ];
+}
 if ( isset($model->data['message']) && (!empty($model->data['id']) || !empty($model->data['users'])) ){
   if ( empty($model->data['id']) ){
     $chat = new \bbn\appui\chat($model->db, $model->inc->user);
