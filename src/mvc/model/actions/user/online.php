aa53319ac48588b58a28f8e5577c6ad6cf811486
Mirko Argentino <mirko@bbn.solutions>
2020-10-20 18:31:01 +0200

    Poller model and actions
    Poller model
    Online action
    Offline action

---
+++ src/mvc/model/actions/user/online.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BBN
+ * Date: 24/10/2018
+ * Time: 14:37
+ */
+
+return [
+  'success' => $model->inc->chat->set_online()
+];
--- src/plugins/appui-core/model/poller.php
+++ src/plugins/appui-core/model/poller.php
@@ -1,76 +1,115 @@
 <?php
-$user_system = new \bbn\user\users($model->db);
-$chat_system = new \bbn\appui\chat($model->db, $model->inc->user);
+$cc = new \bbn\appui\chat($model->db, $model->inc->user);
 return [[
   'id' => 'appui-core-1',
   'frequency' => 1,
-  'function' => function(array $data) use($chat_system){
+  'function' => function(array $data) use($cc){
     $res = [];
-    if ( !empty($data['enabled']) ){
-      $ctmp = [
-        'current' => [],
-        'last' => $data['lastChat'] ?? 0
+    $online = $cc->get_user_status();
+    if ($online !== $data['online']) {
+      $res = [
+        'online' => $online,
+        'serviceWorkers' => [
+          'online' => $online
+        ]
       ];
-      if ($chats = $chat_system->get_chats()) {
-        foreach ($chats as $c) {
-          $ctmp['current'][$c] = [
-            'info' => $chat_system->info($c),
-            'admins' => $chat_system->get_admins($c),
-            'participants' => $chat_system->get_participants($c, false)
-          ];
-        }
+    }
+    if (!empty($online)) {
+      $chats_hash = $cc->get_chats_hash();
+      $chats = $cc->get_chats();
+      if (empty($data['lastChat'])) {
+        $data['lastChat'] = null;
       }
-      $chats_hash = md5(json_encode($ctmp));
-      if ( ($chats_hash !== $data['chatsHash']) ){
-        $ctmp['hash'] = $chats_hash;
-        foreach ( $chats as $c ){
-          if ( empty($data['chatsHash']) ){
-            if ( $m = $chat_system->get_prev_messages($c) ){
-              $ctmp['current'][$c]['messages'] = $m;
-              $max = $m[count($m)-1]['time'];
-              if (\bbn\x::compare_floats($max, $ctmp['last'], '>')) {
-                $ctmp['last'] = $max;
-              }
-            }
-          }
-          else if ( $m = $chat_system->get_next_messages($c, $data['lastChat'] ?? null) ){
-            $ctmp['current'][$c]['messages'] = $m;
+      $last = $data['lastChat'] ?? 0;
+      if ($chats_hash !== $data['chatsHash']) {
+        $res['chats'] = [
+          'current' => [],
+          'hash' => $chats_hash
+        ];
+        foreach ($chats as $c) {
+          $res['chats']['current'][$c] = [
+            'info' => $cc->info($c),
+            'admins' => $cc->get_admins($c),
+            'participants' => $cc->get_participants($c, false, true)
+          ];
+          if ($m = empty($data['chatsHash']) ? $cc->get_prev_messages($c) : $cc->get_next_messages($c, $data['lastChat'])) {
+            $res['chats']['current'][$c]['messages'] = $m;
             $max = $m[count($m)-1]['time'];
-            if ( \bbn\x::compare_floats($max, $ctmp['last'], '>')) {
-              $ctmp['last'] = $max;
+            if (\bbn\x::compare_floats($max, $last, '>')) {
+              $last = $max;
             }
           }
         }
-        $res = [
-          'chats' => $ctmp,
-          'serviceWorkers' => [
-            'chatsHash' => $chats_hash,
-            'lastChat' => $ctmp['last']
-          ]
+        $res['serviceWorkers'] = [
+          'chatsHash' => $chats_hash
         ];
       }
+      else if (!empty($data['lastChat'])) {
+        foreach ($chats as $c) {
+          if ($m = $cc->get_next_messages($c, $data['lastChat'])) {
+            if (!isset($res['messages'])) {
+              $res['messages'] = []; 
+            }
+            $res['messages'][$c] = $m;
+            $max = $m[count($m)-1]['time'];
+            if (\bbn\x::compare_floats($max, $last, '>')) {
+              $last = $max;
+            }
+          }
+        }
+      }
+      if (!empty($res['chats']) || !empty($res['messages'])) {
+        $res['last'] = !empty($chats_hash) ? $last : null;
+        if (!isset($res['serviceWorkers'])) {
+          $res['serviceWorkers'] = [];
+        }
+        $res['serviceWorkers']['lastChat'] = $res['last'];
+      }
+    }
+    else if (empty($data['lastChat'])
+      && ($max = $cc->get_max_last_activity())
+      && ($chats_hash = $cc->get_chats_hash($max))
+      && ($chats_hash !== $data['chatsHash'])
+      && ($chats = $cc->get_chats($max))
+    ) {
+      $res['last'] = $max;
+      $res['chats'] = [
+        'current' => [],
+        'hash' => $chats_hash
+      ];
+      foreach ($chats as $c) {
+        $res['chats']['current'][$c] = [
+          'info' => $cc->info($c),
+          'admins' => $cc->get_admins($c),
+          'participants' => $cc->get_participants($c, false, true),
+          'messages' => $cc->get_prev_messages($c, $max)
+        ];
+      }
+      if (!isset($res['serviceWorkers'])) {
+        $res['serviceWorkers'] = [];
+      }
+      $res['serviceWorkers']['lastChat'] = $max;
+      $res['serviceWorkers']['chatsHash'] = $chats_hash;
     }
     return $res;
   }
 ], [
   'id' => 'appui-core-2',
   'frequency' => 10,
-  'function' => function(array $data) use($user_system){
+  'function' => function(array $data) use($cc){
     $res = [];
-    if ( !empty($data['enabled']) ){
-      $users = $user_system->online_list();
-      $users_hash = md5(json_encode($users));
-      if ($users_hash !== $data['usersHash']) {
-        $res = [
-          'users' => [
-            'list' => $users,
-            'hash' => $users_hash
-          ],
-          'serviceWorkers' => [
-            'usersHash' => $users_hash
-          ]
-        ];
-      }
+    $users = $cc->get_online_users();
+    $users_hash = md5(json_encode($users));
+    if ($users_hash !== $data['usersHash']) {
+      $res = [
+        'users' => [
+          'list' => $users,
+          'hash' => $users_hash
+        ],
+        'serviceWorkers' => [
+          'usersHash' => $users_hash
+        ]
+      ];
     }
     return $res;
   }
