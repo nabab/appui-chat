15f5fd7baf89f7ea0264ef423386baf19d28a801
nabab <thomas.nabet@gmail.com>
2024-03-25 00:00:14 +0100

    appui transformations

--- src/mvc/js/home.js
+++ src/mvc/js/home.js
@@ -3,7 +3,7 @@
   return {
     data(){
       return {
-        allUsers: appui.app.users,
+        allUsers: appui.users,
         currentFilter: '',
         usersTimeout: 0,
         online: []
@@ -11,11 +11,11 @@
     },
     computed: {
       users(){
-        return appui.app.users.filter((a) => {
+        return appui.users.filter((a) => {
           if ( this.currentFilter && (a.text.indexOf(this.currentFilter) === -1) ){
             return false;
           }
-          return a.value !== appui.app.user.id;
+          return a.value !== appui.user.id;
         })
       }
     },
