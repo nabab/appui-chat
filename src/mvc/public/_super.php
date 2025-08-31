abc7b06fe2a5eda9bb9d3c355f28153c69f62821
nabab <thomas.nabet@gmail.com>
2025-09-01 01:49:48 +0200

    Fixed comments

--- src/mvc/public/_super.php
+++ src/mvc/public/_super.php
@@ -1,10 +1,6 @@
 <?php
-/*
- * Describe what it does!
- *
- **/
 
-/** @var $this \bbn\Mvc\Controller */
+/** @var bbn\Mvc\Controller $ctrl */
 if ( !\defined('APPUI_CHAT_ROOT') ){
   define('APPUI_CHAT_ROOT', $ctrl->pluginUrl('appui-chat').'/');
   $ctrl->addInc('chat', new \bbn\Appui\Chat($ctrl->db, $ctrl->inc->user));
--- src/mvc/public/home.php
+++ src/mvc/public/home.php
@@ -1,10 +1,6 @@
 <?php
-/*
- * Describe what it does!
- *
- **/
 
-/** @var $this \bbn\Mvc\Controller */
+/** @var bbn\Mvc\Controller $ctrl */
 $ctrl
   ->setIcon('nf nf-fa-stack_exchange')
   ->setColor('#3B5998', '#FFF')
